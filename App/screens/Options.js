fb1064ea70f589d8f2257fce29af6c2ab2cdf289
KhanhPham2411 <phamduckhanh2411@gmail.com>
2021-04-02 15:52:17 +0700

    link

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,36 +1,99 @@
 import React from 'react';
-import { SafeAreaView} from 'react-native';
+import { SafeAreaView, ScrollView, Linking, Alert} from 'react-native';
 import {Entypo} from '@expo/vector-icons';
 
 import colors from '../constants/colors';
 import { RowItem, RowSeparator } from '../components/RowItem';
 
+//
+
+
+const openUrl = (url) =>{
+  return Linking.openURL(url).catch(() => {
+    Alert.alert('Sorry, something went wrong.', 'Please try again later');
+  });
+};
 
 export default () => {
   return (
-    <SafeAreaView style={{paddingTop: 50,}}>
+    <SafeAreaView style={{paddingTop: 50, height: 400}}>
+      <ScrollView>
+        <RowItem 
+          text='Themes'
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="chevron-right" size={20} color={colors.blue} />} 
+        />
 
-      <RowItem 
-        text='Themes'
-        onPress={() => alert('todo!')}
-        rightIcon={<Entypo name="chevron-right" size={20} color={colors.blue} />} 
-      />
+        <RowSeparator />
 
-      <RowSeparator />
+        <RowItem 
+          text='React Native Basics'
+          onPress={() => openUrl('http://www.handlebarlabs.com/')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+        />
 
-      <RowItem 
-        text='React Native Basics'
-        onPress={() => alert('todo!')}
-        rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
-      />
-
-      <RowSeparator />
-      
-      <RowItem 
-        text='React Native Example'
-        onPress={() => alert('todo!')}
-        rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
-      />
+        <RowSeparator />
+        
+        <RowItem 
+          text='Example'
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+        />
+        <RowSeparator />
+        <RowItem 
+          text='Example'
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+        />
+        <RowSeparator />
+        <RowItem 
+          text='Example'
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+        />
+        <RowSeparator />
+        <RowItem 
+          text='Example'
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+        />
+        <RowSeparator />
+        <RowItem 
+          text='Example'
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+        />
+        <RowSeparator />
+        <RowItem 
+          text='Example'
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+        />
+        <RowSeparator />
+        <RowItem 
+          text='Example'
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+        />
+        <RowSeparator />
+        <RowItem 
+          text='Example'
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+        />
+        <RowSeparator />
+        <RowItem 
+          text='Example'
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+        />
+        <RowSeparator />
+        <RowItem 
+          text='Example'
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />} 
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };

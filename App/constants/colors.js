4776adab265381d8f77c2fbfb08373c0749d1083
KhanhPham2411 <phamduckhanh2411@gmail.com>
2021-04-12 13:56:08 +0700

    custom input component

--- App/constants/colors.js
+++ App/constants/colors.js
@@ -2,4 +2,6 @@ export default {
   text: '#343434',
   border: '#E2E2E2',
   blue: '#4f6d7a',
+  white: '#fff',
+  textLigt: '#979797',
 };
--- App/screens/Home.js
+++ App/screens/Home.js
@@ -3,14 +3,33 @@ import {
   View,
   StyleSheet,
   StatusBar,
+  Image,
+  Dimensions,
 } from 'react-native';
+import { ConversionInput } from '../components/ConversionInput';
 
 import colors from '../constants/colors';
+
+const screen = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.blue,
     flex: 1,
+    justifyContent: "center",
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoBackGround: {
+    width: screen.width*0.45,
+    height: screen.height*0.45,
+  },
+  logo: {
+    position: 'absolute',
+    width: screen.width*0.25,
+    height: screen.height*0.25,
   },
 });
 
@@ -18,6 +37,24 @@ export default () => {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor={colors.blue} />
+
+      <View style={styles.logoContainer}>
+        <Image source={require('../assets/images/background.png')} style={styles.logoBackGround} resizeMode='contain' />
+        <Image source={require('../assets/images/logo.png')} style={styles.logo} resizeMode='contain' />
+      </View>
+
+      <ConversionInput
+        text="USD"
+        onButtonPress={()=> alert("todo!")}
+        value="123"
+        onChangeText={(text) => console.log("text", text)}
+        keyboardType="numeric"
+      />
+      <ConversionInput
+        text="GBP"
+        onButtonPress={()=> alert("todo!")}
+        value="123"
+      />
     </View>
   );
 };
